64a85f1cb54e481282fd5c9e7ed1604d9cbf5109
Daria Sobon <da.sobon@gmail.com>
2018-02-24 09:57:09 +0700

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+
+var a = 5;
+var b = 8;
+
+var value = (a * a) - (2 * a * b) - (b * b);
+console.log(value);
+if (value > 0) {
+	console.log('Wynik dodatni');
+} else if (value < 0) {
+	console.log('Wynik ujemny');
+} else {
+	console.log('Wynik równy 0');
+}
+
+
+
+/*
+var a = 5;
+var b = 6;
+var value = (a * a) - (2 * a * b) - (b * b);
+console.log(value);
+if (value > 0) {
+    console.log('Wynik dodatni');
+}
+else if (value < 0) {
+    console.log('Wynik ujemny');
+}
+else {
+    console.log('Wynik równy 0');
+}
+
+*/
